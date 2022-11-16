991dfdc157be071c0342f16980243f90aeb47ddd
craig-dsilva <craig.s.dsilva@gmail.com>
2022-11-16 17:57:47 +0000

    Continous quiz

--- src/containers/Quiz.tsx
+++ src/containers/Quiz.tsx
@@ -1,28 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import Guess from '../components/Guess';
 
-import initialData from '../data/capitals.json';
+import Countries from '../data/capitals.json';
 
 const Quiz = () => {
-  // const [countries, setCountries] = useState(initialData);
   const [options, setOptions] = useState<any>([]);
-  const [currentCountry, setCurrentCountry] = useState(initialData[0]);
+  const [currentCountry, setCurrentCountry] = useState<any>(
+    Countries[Math.round(Math.random() * Countries.length - 1)]
+  );
   const [correctText, setCorrectText] = useState(false);
   const [incorrectText, setIncorrectText] = useState(false);
 
   useEffect(() => {
     const arr = [];
     for (let i = 0; i < 4; i++) {
-      arr.push(initialData[i]);
+      arr.push(Countries[Math.round(Math.random() * Countries.length - 1)]);
     }
+    arr.push(currentCountry);
     setOptions([...options, ...arr]);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [currentCountry]);
 
   const checkAnswer = (capital: string) => {
     if (capital === currentCountry.capital) {
       setCorrectText(true);
       setTimeout(() => setCorrectText(false), 2000);
+      setCurrentCountry(
+        Countries[Math.round(Math.random() * Countries.length - 1)]
+      );
+      setOptions([]);
     } else {
       setIncorrectText(true);
       setTimeout(() => setIncorrectText(false), 2000);
@@ -41,8 +47,7 @@ const Quiz = () => {
           />
         )
       )}
-      {incorrectText && <p>Incorrect !</p>}
-      {correctText && <p>Correct !</p>}
+      {incorrectText ? <p>Incorrect!</p> : correctText && <p>Correct!</p>}
     </div>
   );
 };
