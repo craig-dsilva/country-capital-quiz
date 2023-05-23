8699bfe35a8154c9c710b43ba7fe675e47e69bfc
craig-dsilva <craig.s.dsilva@gmail.com>
2023-05-23 11:50:38 +0100

    Correct text colour change

--- src/containers/Quiz.tsx
+++ src/containers/Quiz.tsx
@@ -65,7 +65,7 @@ const Quiz: React.FC<QuizInterface> = ({
       </div>
       <p
         style={{
-          color: '#0f0',
+          color: '#2cbf2c',
           visibility: !correctText ? 'hidden' : 'visible',
         }}
       >
